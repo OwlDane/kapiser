94dd6f305737cd665163a5ed47115906a233d266
OwlDane <145408875+OwlDane@users.noreply.github.com>
2023-11-11 18:01:53 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'bukutamu';
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
-mysqli_select_db($conn, $dbname);
-?>
